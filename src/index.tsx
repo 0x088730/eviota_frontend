d6d55afaa0ffc64f6840400ea850695935fad665
0x088730 <royalsceptre88@gmail.com>
2022-12-18 23:04:42 -0800

    update-comment

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 import { Auth0Provider } from '@auth0/auth0-react'
 import './bootstrap-eviota.css'
-// import 'bootstrap/dist/css/bootstrap.min.css'
 import { MixpanelProvider } from 'react-mixpanel-browser';
 import { ApolloWrapper } from './components/apollo-wrapper'
 import { KinProvider } from './lib/kin-client'
